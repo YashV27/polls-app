86d03e0b87ce8d2a2a587b40111e2f65437afc7f
Yash <yash.vidyasagar.279@gmail.com>
2015-05-10 13:59:02 +0530

    Fixed some syntax errors.Everything working awesome-ly

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/',include('polls.urls')),
+	url(r'^admin/',include(admin.site.urls)),
 ]
 
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hi,You've reached polls indec page")
+    return HttpResponse("Hi,You've reached polls index page")
 
 def detail(request,question_id):
     return HttpResponse("You're looking at question %s" %question_id)
